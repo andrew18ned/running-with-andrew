b55f35fc5c53e883fb0c960cd9857dfe08945d07
andrew18ned <andrej.nedilskej@gmail.com>
2022-06-04 21:23:15 +0300

    builup db postgress. connect and create fields. create super user in postresql

--- running/running/settings.py
+++ running/running/settings.py
@@ -55,16 +55,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'running.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'run',
+        'USER': 'user_run',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
